fda8d4edae5ad4e3ffd2c7faef9a46b20738fa6c
Raymond Neilson <rneilson@interloper.ca>
2016-04-21 01:10:57 -0400

    Client detail working

--- webview/webview.js
+++ webview/webview.js
@@ -4,7 +4,7 @@ var iwsApp = angular.module('iws-webview', []);
 
 iwsApp.run()
 
-iwsApp.factory('authService', ['$http', function ($http) {
+iwsApp.factory('authService', ['$http', '$q', function ($http, $q) {
 	var authurl = '/featreq/auth/';
 	var status = {
 		logged_in: false,
@@ -12,35 +12,42 @@ iwsApp.factory('authService', ['$http', function ($http) {
 		full_name: "",
 		csrf_token: "",
 	};
+	var update = function (response) {
+		// Update auth status
+		status.logged_in = response.data.logged_in;
+		status.username = response.data.username;
+		status.full_name = response.data.full_name;
+		status.csrf_token = response.data.csrf_token;
+		// Update default POST header
+		$http.defaults.headers.post['X-CSRFToken'] = status.csrf_token;
+		return status;
+	}
+	var failed = function (reason) {
+		// TODO: show error message
+		return reason.data;
+	}
 	var refresh = function () {
-		$http.get(authurl).then(
-			function (response) {
-				// Update auth status
-				newstatus = response.data;
-				status.logged_in = newstatus.logged_in;
-				status.username = newstatus.username;
-				status.full_name = newstatus.full_name;
-				status.csrf_token = newstatus.csrf_token;
-				// Update default POST header
-				$http.defaults.headers.post['X-CSRFToken'] = newstatus.csrf_token
-			},
-			function (reason) {
-				// Show error message
-			}
-		);
+		return $http.get(authurl).then(update, failed);
 	};
-	var login = function (username, password) {};
+	var login = function (username, password) {
+		return $http.post(authurl, {
+			action: "login",
+			username: username,
+			password: password
+		}).then(update, failed);
+	};
+	refresh();
 	return {
 		status: status,
 		refresh: refresh,
 		login: login
-	}
+	};
 }]);
 
 iwsApp.factory('clientListService', ['authService', '$http', function (authService, $http) {
-	var clienturl = '/featreq/client/'
+	var clienturl = '/featreq/client/';
 	var getclients = function () {
-		return $http.get(clienturl).then(function(response) {
+		return $http.get(clienturl).then(function (response) {
 			return response.data.client_list;
 		});
 	};
@@ -49,11 +56,55 @@ iwsApp.factory('clientListService', ['authService', '$http', function (authServi
 	};
 }]);
 
-iwsApp.controller('ClientListController', ['$scope', 'clientListService',
-	function ($scope, clientListService) {
-		clientListService.getclients().then(function(client_list) {
-			$scope.client_list = client_list;
+iwsApp.factory('clientDetailService', ['$http', function ($http) {
+	var baseurl = '/featreq/client/';
+	var client = {}
+	var getdetails = function (client_id) {
+		return $http.get(baseurl + client_id).then(function (response) {
+			newclient = response.data.client;
+			client.name = newclient.name;
+			client.con_name = newclient.con_name;
+			client.con_mail = newclient.con_mail;
+			client.date_add = new Date(newclient.date_add).toDateString();
+			return client;
 		});
+	};
+	return {
+		client: client,
+		getdetails: getdetails
+	};
+}]);
+
+iwsApp.factory('selectService', function () {
+	return {
+		client_id: "",
+		req_id: ""
+	};
+});
+
+iwsApp.controller('AuthController', ['$scope', 'authService', 
+	function ($scope, authService) {
+		// authService.refresh().then(function(status) {
+			// $scope.username = status.username;
+		// });
+		$scope.status = authService.status;
+		// TODO: add refresh timer, login func
 	}
 ]);
 
+iwsApp.controller('ClientListController', ['$scope', 'clientListService', 'clientDetailService',
+	function ($scope, clientListService, clientDetailService) {
+		clientListService.getclients().then(function (client_list) {
+			$scope.client_list = client_list;
+		});
+		this.selectclient = function (client_id) {
+			clientDetailService.getdetails(client_id);
+		};
+	}
+]);
+
+iwsApp.controller('ClientDetailController', ['$scope', 'clientDetailService',
+	function ($scope, clientDetailService) {
+		$scope.client = clientDetailService.client;
+	}
+]);
